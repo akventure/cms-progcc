559ed4814067c5af377f5bc2f02f2778f80a1812
Danish Mehmood <danishm7012@gmail.com>
2021-07-17 12:58:36 +0500

    add meta fields

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST"),
+        host: env("DATABASE_HOST", "progcc.4u7s7.mongodb.net"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME"),
-        username: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
+        database: env("DATABASE_NAME", "pro-cms"),
+        username: env("DATABASE_USERNAME", "Admin"),
+        password: env("DATABASE_PASSWORD", "admin"),
       },
       options: {
         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
